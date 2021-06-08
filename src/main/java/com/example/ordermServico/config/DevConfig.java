35686ef2fda6d59c63f133f8887ba7353725576f
ANA KAROLINA SANTOS OLIVEIRA <anna15soliveira@gmail.com>
2021-06-08 08:35:25 -0300

    Perfio dev postgresql

---
+++ src/main/java/com/example/ordermServico/config/DevConfig.java
@@ -0,0 +1,25 @@
+package com.example.ordermServico.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.example.ordermServico.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		dbService.instantiateDatabase();
+		return true;
+	}
+
+}
